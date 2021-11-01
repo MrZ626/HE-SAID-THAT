f9b0d87ea5a60ef5b98f50e58f56f0d4bc293d37
MrZ626 <1046101471@qq.com>
2021-11-01 17:19:52 +0800

    使用空格缩进

--- main.lua
+++ main.lua
@@ -15,65 +15,65 @@ You need create list.lua like this:
 /* list.lua */
 local newImg=love.graphics.newImage
 return{
-	{
-		head=newImg("head.png"),
-		bar=newImg("body.png"),
-		tail=newImg("tail.png"),
-		textColor={.2,.2,.2},
-		headX=180,	--barX
-		headY=75,	--barY
-		textX=170,	--stringX
-		textY=95,	--stringY
-		tailY=75,	--tailY
-		tailLen=30,	--tailLen
-	},
-	{
-		...
-	},
-	...
+    {
+        head=newImg("head.png"),
+        bar=newImg("body.png"),
+        tail=newImg("tail.png"),
+        textColor={.2,.2,.2},
+        headX=180,    --barX
+        headY=75,    --barY
+        textX=170,    --stringX
+        textY=95,    --stringY
+        tailY=75,    --tailY
+        tailLen=30,    --tailLen
+    },
+    {
+        ...
+    },
+    ...
 }
 ]]
 
 function love.keypressed(k)
-	if k=="backspace"then
-		local byteoffset = utf8.offset(s,-1)
+    if k=="backspace"then
+        local byteoffset = utf8.offset(s,-1)
         if byteoffset then
             s=string.sub(s,1, byteoffset-1)
         end
-		needDraw=true
-	elseif k=="tab"then
-		sel=sel%#S+1
-		needDraw=true
-	end
+        needDraw=true
+    elseif k=="tab"then
+        sel=sel%#S+1
+        needDraw=true
+    end
 end
 function love.textinput(c)
-	s=s..c
-	needDraw=true
+    s=s..c
+    needDraw=true
 end
 function love.resize()
-	needDraw=true
+    needDraw=true
 end
 function love.run()
-	-- gc.scale(.8)
-	return function()
-		love.event.pump()
-		for name,a in love.event.poll()do
-			if name=="quit"then return 0 end
-			love.handlers[name](a)
-		end
-		if needDraw then
-			t=gc.newText(f,s)
-			local str=S[sel]
-			local barLen=math.max(t:getWidth(),30)-str.tailLen
-			gc.clear(.92,.93,.96)
-			gc.setColor(1,1,1)
-			gc.draw(str.head,20,20)
-			gc.draw(str.bar,str.headX,str.headY,nil,barLen,1)
-			gc.draw(str.tail,str.headX+barLen,str.tailY)
-			gc.setColor(str.textColor)
-			gc.draw(t,str.textX,str.textY)
-			gc.present()
-			needDraw=false
-		end
-	end
+    -- gc.scale(.8)
+    return function()
+        love.event.pump()
+        for name,a in love.event.poll()do
+            if name=="quit"then return 0 end
+            love.handlers[name](a)
+        end
+        if needDraw then
+            t=gc.newText(f,s)
+            local str=S[sel]
+            local barLen=math.max(t:getWidth(),30)-str.tailLen
+            gc.clear(.92,.93,.96)
+            gc.setColor(1,1,1)
+            gc.draw(str.head,20,20)
+            gc.draw(str.bar,str.headX,str.headY,nil,barLen,1)
+            gc.draw(str.tail,str.headX+barLen,str.tailY)
+            gc.setColor(str.textColor)
+            gc.draw(t,str.textX,str.textY)
+            gc.present()
+            needDraw=false
+        end
+    end
 end
